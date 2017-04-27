f651c633ea93df044cad235ffade12bdc8e0157c
muschellij2 <muschellij2@gmail.com>
2017-04-27 10:52:04 -0700

    Added ABOverlap

---
+++ R/afni_3dABoverlap.R
@@ -0,0 +1,39 @@
+#' @title AFNI 3dABoverlap function
+#' @description Wrapper for AFNI \code{3dABoverlap} function
+#'
+#' @param file1 nifti object or NIfTI filename for overlap
+#' @param file2 nifti object or NIfTI filename for overlap
+#' @param automask if \code{TRUE}, \code{automask} is run on the files.  
+#' If \code{FALSE}, consider input datasets as masks 
+#' (automask does not work on mask datasets)
+#' 
+#' @return Output filename of the brik
+#' @export
+afni_3dABoverlap = function(
+  file1, #source
+  file2,
+  automask = TRUE) {
+  
+  func = "3dABoverlap"
+  cmd = get_afni()
+  cmd <- paste0(cmd, func)
+  
+  
+  file1 = checkimg(file1)
+  file2 = checkimg(file2)
+  
+  opts = ""
+  if (!automask) {
+    opts = c(opts, "-no_automask")
+  }
+
+  opts = trimws(opts)
+  opts = opts[ opts != "" ]
+  
+  opts = paste(opts, collapse = " ")
+
+  cmd = paste(cmd, opts, file1, file1)
+  res = system(cmd, intern = TRUE)
+  # res = res[seq(2, length(res))]
+  return(res)
+}
