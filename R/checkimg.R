02807fd54a4884cadc5eb38d6f9575e68d2e467d
muschellij2 <muschellij2@gmail.com>
2018-08-16 17:21:28 -0400

    checkimg for AFNI.

--- R/checkimg.R
+++ R/checkimg.R
@@ -4,12 +4,13 @@
 #' @description Ensures the output to be a character filename (or vector) from an input
 #' image or \code{nifti/afni}.  
 #' @param file character or \code{afni} object
+#' @param allow_array allow arrays to be passed in
 #' @param ... options passed to \code{\link{afni_3dAFNItoNIFTI}} 
 #' @export
 #' @importFrom neurobase checkimg 
 #' @importFrom oro.nifti writeAFNI
 #' @importFrom methods setMethod
-setMethod("checkimg", "afni", function(file, ...) { 
+setMethod("checkimg", "afni", function(file, allow_array = FALSE, ...) { 
   tfile = tempfile()
   writeAFNI(nim = file, fname = tfile)
   res = afni_3dAFNItoNIFTI(file = tfile, outfile = NULL, retimg = FALSE, ...)
